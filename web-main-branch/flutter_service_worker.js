0277553af63e3591b8e9aba2b097aee3821ea4ec
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-02-11 23:08:19 +0100

    Version 0.0.17 build 4 (a0c65fb) of the i-care.by app
    Improvements, comparing to version 0.0.16 of the application:
    visible:
    - introduce a set of statements to prepare a reader to understand
      the statement about state of relationships between the society
      and `officials and special forces`.
    - record my gratitude to the people, who helped in January 2025.
    
    technical:
    - upgrade tools, used to build the application (Flutter to 3.27.4
      and all used packages to the most recent ones).

--- web-main-branch/flutter_service_worker.js
+++ web-main-branch/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "7f50c405c3e8f048e840a386dd0397a9",
+const RESOURCES = {"flutter_bootstrap.js": "01ced2a2a06f58727e6853a5dfd999a8",
 "assets/AssetManifest.json": "b4197e815067d5deaaf6afdd8d61c07a",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/assets/images/how-to-find-out-who-is-tmipiyl-sound-wave-640.jpg": "3a2e3f8c3dadca8ec018c51a47dfe927",
@@ -44,8 +44,8 @@ const RESOURCES = {"flutter_bootstrap.js": "7f50c405c3e8f048e840a386dd0397a9",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "208ad97de276b08cef00b88a40c92db1",
 "assets/AssetManifest.bin.json": "34b8117da95b66d2bc6c7aa5d23d094d",
-"main.dart.js": "b3b6aa5b84727fd87494005f16b35d15",
-"version.json": "4e4570a5e8635ec80b4eecfcd8aff311",
+"main.dart.js": "f5a211910967a8bdf8d8c7dae931ccb6",
+"version.json": "97d5cf28350066db58412cda8270c044",
 "canvaskit/skwasm.js.symbols": "96263e00e3c9bd9cd878ead867c04f3c",
 "canvaskit/canvaskit.wasm": "e7602c687313cfac5f495c5eac2fb324",
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
